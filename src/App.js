8e04dfbebefef704f3478a49858ea503fdd9c6eb
ShikharBansal <shikharbansal007@gmail.com>
2021-06-13 18:06:11 +0530

    Done with table and worldwide data

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ import {
 } from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
+import Table from "./Table";
+import { sortData } from "./util";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("Worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -34,6 +37,9 @@ function App() {
             value: country.countryInfo.iso2,
           }));
 
+          const sortedData = sortData(data);
+
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -107,6 +113,7 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           {/* Tables */}
+          <Table countries={tableData} />
           <h3>Worldwide new Cases</h3>
           {/* Graphs */}
         </CardContent>
